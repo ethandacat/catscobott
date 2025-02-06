15f68f8684c6e09af1ae774b0835a828b48230b4
Ethan <pointlessx@duck.com>
2025-02-05 15:07:36 -1000

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ CATSCOBOT-SOURCE
 while True:
   try:
     code = requests.get("https://raw.githubusercontent.com/ethandacat/catscobott/refs/heads/main/prod.py").text
-    print("Version Number: "+"".join(code.split("#")).split("\n")[0])
+    print("Version Number: "+code.split("#")[0].split("\n")[0])
     exec(code)
   except:
     print("""
